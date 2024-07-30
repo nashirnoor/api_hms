b3ae95bfccec26d2e6880ec42929403c2d33bd05
nashir <nashirnoor1718@gmail.com>
2024-07-30 16:53:59 +0530

    .v3

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Department, Doctor, Patient, PatientRecord
+
+admin.site.register(User)
+admin.site.register(Department)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(PatientRecord)
